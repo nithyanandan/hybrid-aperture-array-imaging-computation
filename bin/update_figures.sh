333e66f83e6b8e391c935a0cbad82812c90762c2
Nithyanandan Thyagarajan <nithyanandan.t@gmail.com>
2024-10-24 17:59:16 +0100

    Renumber figures in ms.tex and figures script

--- bin/update_figures.sh
+++ bin/update_figures.sh
@@ -22,24 +22,24 @@ rsync -havux --progress figures/1D_coherent_per_pixel_compcost_analysis_SKA1-low
 rsync -havux --progress figures/1D_coherent_per_pixel_compcost_analysis_CASPA.pdf             1D_coherent_per_pixel_compcost_analysis_CASPA.pdf                                 
 rsync -havux --progress figures/1D_coherent_per_pixel_compcost_analysis_FarView.pdf           1D_coherent_per_pixel_compcost_analysis_FarView.pdf                               
 
-mv aperture_image_dimensions.pdf                                           fig_00.pdf
-mv flops_calc_abridged_v2.pdf                                              fig_01.pdf
-mv 1D_beamformer_dft_per_pixel_cost_analysis_LAMBDA-I.pdf                  fig_02.pdf
-mv multidim_beamformer_dft_per_pixel_cost_analysis_LAMBDA-I.pdf            fig_03.pdf
-mv 1D_epic_per_pixel_cost_analysis_LAMBDA-I.pdf                            fig_04.pdf
-mv multidim_epic_per_pixel_cost_analysis_LAMBDA-I.pdf                      fig_05a.pdf
-mv multidim_epic_relative_cost_analysis_LAMBDA-I.pdf                       fig_05b.pdf 
-mv 1D_xcor_dft_per_pixel_cost_analysis_LAMBDA-I.pdf                        fig_06.pdf
-mv multidim_xcor_dft_per_pixel_cost_analysis_LAMBDA-I.pdf                  fig_07a.pdf
-mv multidim_xcor_dft_relative_cost_analysis_LAMBDA-I.pdf                   fig_07b.pdf  
-mv 1D_xcor_fft_per_pixel_cost_analysis_LAMBDA-I.pdf                        fig_08.pdf
-mv multidim_xcor_fft_per_pixel_cost_analysis_LAMBDA-I.pdf                  fig_09a.pdf  
-mv multidim_xcor_fft_relative_cost_analysis_LAMBDA-I.pdf                   fig_09b.pdf   
-mv 1D_per_pixel_compcost_analysis_LAMBDA-I.pdf                             fig_10a.pdf 
-mv 1D_per_pixel_compcost_analysis_CASPA.pdf                                fig_10b.pdf 
-mv 1D_per_pixel_compcost_analysis_FarView.pdf                              fig_10c.pdf
-mv 1D_coherent_per_pixel_compcost_analysis_LAMBDA-I.pdf                    fig_11a.pdf  
-mv 1D_coherent_per_pixel_compcost_analysis_SKA1-low-core.pdf               fig_11b.pdf
-mv 1D_coherent_per_pixel_compcost_analysis_SKA1-low.pdf                    fig_11c.pdf
-mv 1D_coherent_per_pixel_compcost_analysis_CASPA.pdf                       fig_11d.pdf
-mv 1D_coherent_per_pixel_compcost_analysis_FarView.pdf                     fig_11e.pdf 
+mv aperture_image_dimensions.pdf                                           fig_01.pdf
+mv flops_calc_abridged_v2.pdf                                              fig_02.pdf
+mv 1D_beamformer_dft_per_pixel_cost_analysis_LAMBDA-I.pdf                  fig_03.pdf
+mv multidim_beamformer_dft_per_pixel_cost_analysis_LAMBDA-I.pdf            fig_04.pdf
+mv 1D_epic_per_pixel_cost_analysis_LAMBDA-I.pdf                            fig_05.pdf
+mv multidim_epic_per_pixel_cost_analysis_LAMBDA-I.pdf                      fig_06a.pdf
+mv multidim_epic_relative_cost_analysis_LAMBDA-I.pdf                       fig_06b.pdf 
+mv 1D_xcor_dft_per_pixel_cost_analysis_LAMBDA-I.pdf                        fig_07.pdf
+mv multidim_xcor_dft_per_pixel_cost_analysis_LAMBDA-I.pdf                  fig_08a.pdf
+mv multidim_xcor_dft_relative_cost_analysis_LAMBDA-I.pdf                   fig_08b.pdf  
+mv 1D_xcor_fft_per_pixel_cost_analysis_LAMBDA-I.pdf                        fig_09.pdf
+mv multidim_xcor_fft_per_pixel_cost_analysis_LAMBDA-I.pdf                  fig_10a.pdf  
+mv multidim_xcor_fft_relative_cost_analysis_LAMBDA-I.pdf                   fig_10b.pdf   
+mv 1D_per_pixel_compcost_analysis_LAMBDA-I.pdf                             fig_11a.pdf 
+mv 1D_per_pixel_compcost_analysis_CASPA.pdf                                fig_11b.pdf 
+mv 1D_per_pixel_compcost_analysis_FarView.pdf                              fig_11c.pdf
+mv 1D_coherent_per_pixel_compcost_analysis_LAMBDA-I.pdf                    fig_12a.pdf  
+mv 1D_coherent_per_pixel_compcost_analysis_SKA1-low-core.pdf               fig_12b.pdf
+mv 1D_coherent_per_pixel_compcost_analysis_SKA1-low.pdf                    fig_12c.pdf
+mv 1D_coherent_per_pixel_compcost_analysis_CASPA.pdf                       fig_12d.pdf
+mv 1D_coherent_per_pixel_compcost_analysis_FarView.pdf                     fig_12e.pdf 
