fa9edf8ddb2cd6a99b7b25ebe5813c2bfc0286bd
Nithyanandan Thyagarajan <nithyanandan.t@gmail.com>
2024-10-25 03:21:25 +0100

    Update bin/tgz_manuscript.sh

--- bin/tgz_manuscript.sh
+++ bin/tgz_manuscript.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-tar -czvf manuscript.tar.gz ms.tex ms.bbl ms.pdf refs.bib aas-macros.sty apj.bst cup-journal.cls fig_01.pdf fig_02.pdf fig_03.pdf fig_04.pdf fig_05a.pdf fig_05b.pdf fig_06.pdf fig_07a.pdf fig_07b.pdf fig_08.pdf fig_09a.pdf fig_09b.pdf fig_10a.pdf fig_10b.pdf fig_10c.pdf fig_11a.pdf fig_11b.pdf fig_11c.pdf fig_11d.pdf fig_11e.pdf 
+tar -czvf manuscript.tar.gz ms.tex ms.bbl ms.pdf refs.bib aas-macros.sty apj.bst cup-journal.cls fig_01.pdf fig_02.pdf fig_03.pdf fig_04.pdf fig_05.pdf fig_06a.pdf fig_06b.pdf fig_07.pdf fig_08a.pdf fig_08b.pdf fig_09.pdf fig_10a.pdf fig_10b.pdf fig_11a.pdf fig_11b.pdf fig_11c.pdf fig_12a.pdf fig_12b.pdf fig_12c.pdf fig_12d.pdf fig_12e.pdf 
